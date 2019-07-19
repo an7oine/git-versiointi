987187a2e0c91332892433312eaec931b2532d50
Antti Hautaniemi <antti.hautaniemi@pispalanit.fi>
2019-07-19 11:46:13 +0300

    Päivitetty leimojen haku ja kehitysversioiden tunnistus ja numerointi

--- versiointi/repo.py
+++ versiointi/repo.py
@@ -1,21 +1,33 @@
 # -*- coding: utf-8 -*-
 
+import pkg_resources
+import re
+
 from git.exc import InvalidGitRepositoryError
 from git import Repo
 
+VERSIO = re.compile(r'^v[0-9]', flags=re.IGNORECASE)
+KEHITYSVERSIO = re.compile(r'(.+[a-z])([0-9]*)$', flags=re.IGNORECASE)
 
-def _muotoile_versio(versio, aliversio, *, leima, etaisyys, **kwargs):
+
+
+def _muotoile_versio(leima, etaisyys, versio=None, aliversio=None):
   '''
-  Poista löytyneen leiman alusta merkit `Vv-_.` ja muotoile
-  mahdollinen aliversio määritetyn käytännön mukaisesti
-  ja lisää mahdollinen git-muutoshistorian pituus aliversiona.
+  Määritä versionumero käytännön mukaisesti versiolle, kehitysversiolle ja aliversiolle.
 
   Args:
-    leima (str): lähin git-leima (tag)
+    leima (git.Tag): lähin git-leima (tag)
     etaisyys (int): muutosten lukumäärä leiman jälkeen
-    versio (str): version numerointi, oletus `"{leima}"`
-    aliversio (str): aliversion numerointi, oletus `"{leima}.{etaisyys}"`
+    versio (callable / str): version numerointi, oletus `"{leima}"`
+    aliversio (callable / str): aliversion numerointi, oletus `"{leima}.{etaisyys}"`
   '''
+  if leima is not None:
+    kehitysversio = KEHITYSVERSIO.match(str(leima))
+    if kehitysversio:
+      if kehitysversio.group(2):
+        etaisyys += int(kehitysversio.group(2))
+      return f'{kehitysversio.group(1)}{etaisyys}'
+
   if not callable(versio):
     assert not versio or isinstance(versio, str)
     versio = (versio or '{leima}').format
@@ -24,11 +36,32 @@ def _muotoile_versio(versio, aliversio, *, leima, etaisyys, **kwargs):
     aliversio = (aliversio or '{leima}.{etaisyys}').format
 
   return (aliversio if etaisyys else versio)(
-    leima=str(leima).lstrip('Vv-_.') if leima else '0.0',
+    leima=leima or 'v0.0',
     etaisyys=etaisyys,
-    **kwargs,
   )
   # def _muotoile_versio
+
+
+def git_leima(repo, ref, kehitysversio=False):
+  '''
+  Etsitään ja palautetaan versiojärjestyksessä viimeisin viittaukseen osoittava leima.
+  Ohita kehitysversiot, ellei toisin pyydetä.
+  '''
+  if kehitysversio:
+    suodatin = lambda l: l.commit == ref and VERSIO.match(str(l))
+  else:
+    suodatin = lambda l: (
+      l.commit == ref and VERSIO.match(str(l)) and not KEHITYSVERSIO.match(str(l))
+    )
+  try:
+    return next(iter(sorted(
+      filter(suodatin, repo.tags),
+      key=lambda x: pkg_resources.parse_version(str(x)),
+      reverse=True,
+    )))
+  except StopIteration:
+    return None
+  # def git_leima
 
 
 def git_historia(polku, versio=None, aliversio=None):
@@ -56,55 +89,71 @@ def git_historia(polku, versio=None, aliversio=None):
   for ref in muutokset():
     etaisyys -= 1
 
-    # Jos aiemmin löydetty leima on edelleen käytettävissä,
-    # muodosta versionumero sen perusteella.
+    # Jos aiemmin löydetty leima on edelleen viimeisin löytynyt,
+    # muodosta kehitys- tai aliversionumero sen perusteella.
     if etaisyys > 0:
       yield {
         'tyyppi': 'muutos',
         'tunnus': ref.hexsha,
         'versio': _muotoile_versio(
-          versio, aliversio,
           leima=leima, etaisyys=etaisyys,
+          versio=versio, aliversio=aliversio,
         ),
         'kuvaus': ref.message.rstrip('\n'),
       }
       continue
       # if etaisyys >= 0
 
-    # Jos tälle viittaukselle on suora leima, käytä sitä.
-    leima = repo.git.tag('--points-at', ref.hexsha)
-    if leima:
+    # Etsi mahdollinen julkaistu versiomerkintä ja lisää se
+    # julkaisuna versiohistoriaan.
+    julkaisuleima = git_leima(repo, ref, kehitysversio=False)
+    if julkaisuleima:
       yield {
         'tyyppi': 'julkaisu',
-        'tunnus': repo.tags[leima].object.hexsha,
+        'tunnus': julkaisuleima.object.hexsha,
         'versio': _muotoile_versio(
-          versio, aliversio,
-          leima=leima, etaisyys=0,
+          leima=julkaisuleima, etaisyys=0,
+          versio=versio, aliversio=aliversio,
         ),
-        'kuvaus': getattr(repo.tags[leima].tag, 'message', '').rstrip('\n'),
+        'kuvaus': getattr(julkaisuleima.tag, 'message', '').rstrip('\n'),
       }
+      leima = julkaisuleima
+    # Muutoin ohitetaan julkaisumerkintä ja etsitään uudelleen kehitysversiota.
+    else:
+      julkaisuleima = None
+      leima = git_leima(repo, ref, kehitysversio=True)
+
+    # Jos kehitysversiomerkintä löytyi, lisää sellaisenaan.
+    if leima:
       yield {
         'tyyppi': 'muutos',
         'tunnus': ref.hexsha,
         'versio': _muotoile_versio(
-          versio, aliversio,
           leima=leima, etaisyys=0,
+          versio=versio, aliversio=aliversio,
         ),
         'kuvaus': ref.message.rstrip('\n'),
       }
 
-    else:
+    # Etsi uudelleen mahdollista uudempaa kehitysversiomerkintää,
+    # mikäli kyseessä on lopullinen, julkaistu versio.
+    if julkaisuleima:
+      leima = git_leima(repo, ref, kehitysversio=True)
+
+    # Jos yhtään tähän muutokseen osoittavaa leimaa ei löytynyt,
+    # etsi viimeisin, aiempi (kehitys-) versio ja luo aliversio sen mukaan.
+    if not leima:
       # Etsi lähin leima.
       etaisyys = 1
       for aiempi_ref in ref.iter_parents():
-        leima = repo.git.tag('--points-at', aiempi_ref.hexsha)
+        leima = git_leima(repo, aiempi_ref, kehitysversio=True)
         if leima:
           yield {
             'tyyppi': 'muutos',
             'tunnus': ref.hexsha,
             'versio': _muotoile_versio(
-              versio, aliversio,
               leima=leima, etaisyys=etaisyys,
+              versio=versio, aliversio=aliversio,
             ),
             'kuvaus': ref.message.rstrip('\n'),
           }
@@ -112,14 +161,15 @@ def git_historia(polku, versio=None, aliversio=None):
         etaisyys += 1
         # for aiempi_ref
 
+      # Jos myöskään yhtään aiempaa versiomerkintää ei löytynyt,
+      # muodosta versionumero git-historian pituuden mukaan.
       if not leima:
-        # Jos yhtään leimaa ei löytynyt, palauta git-historian pituus.
         yield {
           'tyyppi': 'muutos',
           'tunnus': ref.hexsha,
           'versio': _muotoile_versio(
-            versio, aliversio,
             leima=None, etaisyys=etaisyys,
+            versio=versio, aliversio=aliversio,
           ),
           'kuvaus': ref.message.rstrip('\n'),
         }
@@ -146,31 +196,40 @@ def git_versio(polku, versio=None, aliversio=None):
   try:
     ref = repo.head.commit
   except ValueError:
-    return '0'
+    return 'v0'
 
-  # Jos HEAD osoittaa suoraan johonkin leimaan, palauta se.
-  leima = repo.git.tag('--points-at', ref.hexsha)
+  # Jos HEAD osoittaa suoraan johonkin julkaisuun, palauta se.
+  leima = git_leima(repo, ref, kehitysversio=False)
   if leima:
     return _muotoile_versio(
-      versio, aliversio,
       leima=leima, etaisyys=0,
+      versio=versio, aliversio=aliversio,
+    )
+
+  # Jos HEAD osoittaa suoraan johonkin kehitysversioon, palauta se.
+  leima = git_leima(repo, ref, kehitysversio=True)
+  if leima:
+    return _muotoile_versio(
+      leima=leima, etaisyys=0,
+      versio=versio, aliversio=aliversio,
     )
 
   # Etsi lähin leima ja palauta määritetyn käytännön mukainen aliversio:
   # oletuksena `leima.n`, missä `n` on etäisyys.
   etaisyys = 1
   for ref in ref.iter_parents():
-    leima = repo.git.tag('--points-at', ref.hexsha)
+    leima = git_leima(repo, ref, kehitysversio=True)
     if leima:
       return _muotoile_versio(
-        versio, aliversio,
         leima=leima, etaisyys=etaisyys,
+        versio=versio, aliversio=aliversio,
       )
     etaisyys += 1
 
-  # Jos yhtään leimaa ei löytynyt, palauta git-historian pituus.
+  # Jos yhtään aiempaa versiomerkintää ei löytynyt,
+  # muodosta versionumero git-historian pituuden mukaan.
   return _muotoile_versio(
-    versio, aliversio,
     leima=None, etaisyys=etaisyys,
+    versio=versio, aliversio=aliversio,
   )
   # def git_versio
