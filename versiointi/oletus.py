b422f5d25b5a0d442f870f1951e065e78435b99b
Antti Hautaniemi <antti.hautaniemi@pispalanit.fi>
2022-06-08 10:52:29 +0300

    Oletuskäytäntö: tulkitaan `v-`-haarat samoin kuin master

--- versiointi/oletus.py
+++ versiointi/oletus.py
@@ -2,6 +2,8 @@
 
 # Oletusversiokäytäntö.
 VERSIOKAYTANTO = {
+  # pylint: disable=line-too-long
+
   # Irtoversio (nk. detached HEAD).
   '*': '''{pohja}+{etaisyys}''',
 
@@ -9,6 +11,11 @@ VERSIOKAYTANTO = {
   # indeksoitu kehitysversio tai haaran mukainen tunniste.
   'refs/heads/ refs/remotes/origin/': \
     '''{pohja}{int(indeksi)+etaisyys if indeksi else f'+{tunnus}.{etaisyys}'}''',
+
+    # Versiohaaran (v-1.2) versio:
+  # indeksoitu kehitysversio tai etäisyyden mukainen pääte.
+  'refs/heads/v-[0-9].* refs/remotes/origin/v-[0-9].*': \
+    '''{pohja}{int(indeksi)+etaisyys if indeksi else f'.{etaisyys}'}''',
 
   # Master-haaran versio:
   # indeksoitu kehitysversio tai etäisyyden mukainen pääte.
