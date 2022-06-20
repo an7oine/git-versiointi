c211637eef2d6d7220b26cafd3924b2d659983ea
Antti Hautaniemi <antti.hautaniemi@pispalanit.fi>
2022-06-20 10:46:55 +0300

    git-1.7.10+ -yhteensopiva symbolien haku

--- versiointi/repo.py
+++ versiointi/repo.py
@@ -7,10 +7,91 @@ from pkg_resources import parse_version
 
 from git.objects.commit import Commit
 from git.objects.tag import TagObject
-from git import Repo
+from git import Git, Repo
 
 
-class Tietovarasto(Repo):
+class LiianVanha(Repo):
+  def __init__(self, *args, **kwargs):
+    raise RuntimeError(
+      'Järjestelmän git-versio on liian vanha.'
+      ' Git 1.7.10+ vaaditaan versioinnin käyttöön.'
+    )
+    # def __init__
+  # class LiianVanha
+
+
+class Repo1_7_10(Repo):
+  ''' Toteutus symboleiden hakuun, git v.1.7.10+. '''
+  def _symboli(self, ref, tyyppi):
+    # Poimitaan erilliset, välilyönnein erotetut tyyppikriteerit.
+    # Muodostetaan mahdollinen säännöllinen lauseke viimeisen tyypin
+    # viimeisen kauttaviivan jälkeisen osan mukaan.
+    tyypit = tyyppi.split(' ') if tyyppi else ()
+    if tyypit and '/' in tyypit[-1]:
+      lauseke = tyypit[-1].rsplit('/', 1)[-1]
+
+    if tyyppi.startswith('refs/tags'):
+      symbolit = self.git.tag('--points-at', ref).split('\n')
+      symbolit = (f'refs/tags/{symboli}' for symboli in symbolit)
+    else:
+      symbolit = self.git.branch(
+        '--contains', ref,
+        '--all',
+      ).split('\n')
+      symbolit = (
+        f'refs/{symboli[2:]}'
+        if '/' in symboli
+        else f'refs/heads/{symboli[2:]}'
+        for symboli in symbolit
+      )
+
+    return filter(
+      re.compile(rf'.*/{lauseke}$').match if lauseke else None,
+      symbolit
+    )
+    # def _etsi_symboli
+  # class Repo1_7_10
+
+
+class Repo2_7_0(Repo):
+  ''' Toteutus symboleiden hakuun, git v.2.7.0+. '''
+  def _symboli(self, ref, tyyppi):
+    # Poimitaan erilliset, välilyönnein erotetut tyyppikriteerit.
+    # Muodostetaan mahdollinen säännöllinen lauseke viimeisen tyypin
+    # viimeisen kauttaviivan jälkeisen osan mukaan.
+    tyypit = tyyppi.split(' ') if tyyppi else ()
+    if tyypit and '/' in tyypit[-1]:
+      lauseke = tyypit[-1].rsplit('/', 1)[-1]
+
+    return filter(
+      re.compile(rf'.*/{lauseke}$').match if lauseke else None,
+      self.git.for_each_ref(
+        # Pyydetään tuloksena ainoastaan viittauksen nimi.
+        '--format=%(refname)',
+
+        # Huomaa, että haara viittaa (nimeämismielessä) paitsi tällä het-
+        # kellä osoittamaansa muutokseen, myös kaikkiin tämän edeltäjiin.
+        # Leima taas viittaa pysyvästi täsmälleen yhteen muutokseen.
+        '--points-at' if tyyppi.startswith('refs/tags') else '--contains', ref,
+
+        # Poimitaan alkuosa (viimeiseen kauttaviivaan saakka) kustakin
+        # annetusta tyypistä.
+        *(tyyppi.rsplit('/', 1)[0] for tyyppi in tyypit),
+      ).split('\n')
+    )
+    # def _etsi_symboli
+  # class Repo2_7_0
+
+
+_git_versio = Git().version_info
+class Tietovarasto(
+  (
+    Repo2_7_0 if _git_versio >= (2, 7, 0)
+    else Repo1_7_10 if _git_versio >= (1, 7, 10)
+    else LiianVanha
+  ),
+  Repo,
+):
   ''' Täydennetty git-tietovarastoluokka. '''
 
   def muutos(self, ref=None):
@@ -52,29 +133,9 @@ class Tietovarasto(Repo):
     except AttributeError: self.symbolit = {}
     except KeyError: pass
 
-    # Poimitaan erilliset, välilyönnein erotetut tyyppikriteerit.
-    # Muodostetaan mahdollinen säännöllinen lauseke viimeisen tyypin
-    # viimeisen kauttaviivan jälkeisen osan mukaan.
-    tyypit = tyyppi.split(' ') if tyyppi else ()
-    if tyypit and '/' in tyypit[-1]:
-      lauseke = tyypit[-1].rsplit('/', 1)[-1]
-
-    symbolit = filter(
-      re.compile(rf'.*/{lauseke}$').match if lauseke else None,
-      self.git.for_each_ref(
-        # Pyydetään tuloksena ainoastaan viittauksen nimi.
-        '--format=%(refname)',
-
-        # Huomaa, että haara viittaa (nimeämismielessä) paitsi tällä het-
-        # kellä osoittamaansa muutokseen, myös kaikkiin tämän edeltäjiin.
-        # Leima taas viittaa pysyvästi täsmälleen yhteen muutokseen.
-        '--points-at' if tyyppi.startswith('refs/tags') else '--contains', ref,
-
-        # Poimitaan alkuosa (viimeiseen kauttaviivaan saakka) kustakin
-        # annetusta tyypistä.
-        *(tyyppi.rsplit('/', 1)[0] for tyyppi in tyypit),
-      ).split('\n')
-    )
+    # Poimitaan pyydetyt symbolit Git-versiokohtaisen
+    # toteutuksen mukaan.
+    symbolit = self._symboli(ref, tyyppi)
 
     # Mikäli yhtään symbolia ei löytynyt, palautetaan `None`.
     # Mikäli löytyi yksi, palautetaan se.
