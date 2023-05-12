2541277a17d06fe36b65172f06dcad3d448b6650
Antti Hautaniemi <antti.hautaniemi@pispalanit.fi>
2023-05-12 13:01:11 +0300

    Oletusversiointi: poistetaan etuliite "v" versioleimasta

--- versiointi/oletus.py
+++ versiointi/oletus.py
@@ -23,10 +23,10 @@ VERSIOKAYTANTO = {
     '''{pohja}{int(indeksi)+etaisyys if indeksi else f'.{etaisyys}'}''',
 
   # Leimattu kehitysversiosarja: tulkitaan viimeinen luku indeksinä.
-  'refs/tags/v[0-9].*': '''{tunnus}{indeksoitu}''',
+  'refs/tags/v[0-9].*': '''{tunnus[1:]}{indeksoitu}''',
 
   # Leimattu versio: käytetään sellaisenaan.
-  'refs/tags/v[0-9][0-9.]*?(?![a-z]+[0-9]*)': '''{tunnus}''',
+  'refs/tags/v[0-9][0-9.]*?(?![a-z]+[0-9]*)': '''{tunnus[1:]}''',
 
   # Nollaversio (edeltää ensimmäistä leimaa).
   '0': '0.0',
