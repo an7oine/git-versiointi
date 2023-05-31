daec29cfcd93b5f6a1f7b95403e5e0f27155eec9
Antti Hautaniemi <antti.hautaniemi@pispalanit.fi>
2023-05-31 15:56:33 +0300

    Täydennetty oletuskäytäntö huomioimaan `v-X.Y`-tyyppiset versiohaarat

--- versiointi/oletus.py
+++ versiointi/oletus.py
@@ -9,23 +9,25 @@ VERSIOKAYTANTO = {
 
   # (Muun kuin master-) haaran versio:
   # indeksoitu kehitysversio tai haaran mukainen tunniste.
-  'refs/heads/ refs/remotes/origin/': \
-    '''{pohja}{int(indeksi)+etaisyys if indeksi else f'+{tunnus}.{etaisyys}'}''',
+  'refs/heads/ refs/remotes/origin/': (
+    '''{pohja}{int(indeksi)+etaisyys if indeksi else f'+{tunnus}.{etaisyys}'}'''
+  ),
 
-    # Versiohaaran (v-1.2) versio:
+  # Master-haara tai versiohaara (v-X.Y):
   # indeksoitu kehitysversio tai etäisyyden mukainen pääte.
-  'refs/heads/v-[0-9].* refs/remotes/origin/v-[0-9].*': \
-    '''{pohja}{int(indeksi)+etaisyys if indeksi else f'.{etaisyys}'}''',
-
-  # Master-haaran versio:
-  # indeksoitu kehitysversio tai etäisyyden mukainen pääte.
-  'refs/heads/master refs/remotes/origin/master': \
-    '''{pohja}{int(indeksi)+etaisyys if indeksi else f'.{etaisyys}'}''',
+  ' '.join((
+    'refs/heads/master',
+    'refs/remotes/origin/master',
+    'refs/heads/v-[0-9].*',
+    'refs/remotes/origin/v-[0-9].*',
+  )): (
+    '''{pohja}{int(indeksi)+etaisyys if indeksi else f'.{etaisyys}'}'''
+  ),
 
   # Leimattu kehitysversiosarja: tulkitaan viimeinen luku indeksinä.
   'refs/tags/v[0-9].*': '''{tunnus[1:]}{indeksoitu}''',
 
-  # Leimattu versio: käytetään sellaisenaan.
+  # Leimattu (ei-kehitys-) versio: käytetään sellaisenaan.
   'refs/tags/v[0-9][0-9.]*?(?![a-z]+[0-9]*)': '''{tunnus[1:]}''',
 
   # Nollaversio (edeltää ensimmäistä leimaa).
