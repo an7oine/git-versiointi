3d581cb3bd412173440f474f6386b14c6efcd266
Antti Hautaniemi <antti.hautaniemi@pispalanit.fi>
2024-12-13 21:59:16 +0200

    Oletuskäytäntö ei-vakioversioille: `<edellinen+1>.dev+<etaisyys>.<tunnus>`

--- versiointi/oletus.py
+++ versiointi/oletus.py
@@ -4,13 +4,13 @@
 VERSIOKAYTANTO = {
   # pylint: disable=line-too-long
 
-  # Irtoversio (nk. detached HEAD).
-  '*': '''{pohja}+{etaisyys}''',
+  # Irtoversio (nk. detached HEAD): lisätään etäisyys.
+  '*': '''{pohja}{int(indeksi)+1 if indeksi else ".1"}.dev+{etaisyys}''',
 
   # (Muun kuin master-) haaran versio:
   # indeksoitu kehitysversio tai haaran mukainen tunniste.
   'refs/heads/ refs/remotes/origin/': (
-    '''{pohja}{int(indeksi)+etaisyys if indeksi else f'+{tunnus}.{etaisyys}'}'''
+    '''{pohja}{int(indeksi)+1 if indeksi else ".1"}.dev+{etaisyys}.{tunnus}'''
   ),
 
   # Master-haara tai versiohaara (v-X.Y):
@@ -19,13 +19,13 @@ VERSIOKAYTANTO = {
     'refs/heads/(master|v-[0-9].*)',
     'refs/remotes/origin/(master|v-[0-9].*)',
   )): (
-    '''{pohja}{int(indeksi)+etaisyys if indeksi else f'.{etaisyys}'}'''
+    '''{pohja}{int(indeksi)+etaisyys if indeksi else f'.{etaisyys}'}{indeksoitu}'''
   ),
 
   # Leimattu kehitysversiosarja: tulkitaan viimeinen luku indeksinä.
   'refs/tags/v[0-9].*': '''{tunnus[1:]}{indeksoitu}''',
 
-  # Leimattu (ei-kehitys-) versio: käytetään sellaisenaan.
+  # Leimattu (ei-kehitys-) versio: poimitaan tunnus, poistetaan "v".
   'refs/tags/v[0-9][0-9.]*?(?![a-z]+[0-9]*)': '''{tunnus[1:]}''',
 
   # Nollaversio (edeltää ensimmäistä leimaa).
